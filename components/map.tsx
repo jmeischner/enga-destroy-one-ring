6d06e5b9d52b4c5a9ec321c57b26c2f686b26a90
Marvin Ruppelt <marvin@ruppelt.ru>
2022-08-08 14:24:17 +0200

    next steps map.tsx

--- components/map.tsx
+++ components/map.tsx
@@ -1,11 +1,20 @@
-import type { NextPage } from 'next'
-
+import { useEffect, useState } from 'react'
 import styles from './styles/Client.module.css'
 
-const Client = (): JSX.Element=> (
-  <div className={styles.map}>
-    Map
- </div>
-)
+const POS_START = [0, 0]
 
+interface MapProps {
+    afterMove: () => void,
+
+}
+
+const Map = (): JSX.Element => {
+    const [currentPos, setCurrentPos] = useState(POS_START)
+    const [historyPos, setHistoryPos] = useState([POS_START])
+    return (
+        <div className={styles.map}>
+            Map
+        </div>
+    )
+}
 export default Client
