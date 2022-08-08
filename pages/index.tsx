ea37ce69ef6d0d3538400834247ae7c17b75da63
Steffen Hofheinz <shofheinz@stroeer.de>
2022-08-08 13:55:46 +0200

    next steps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next'
 
-import Client from '../components/client'
+import Client from 'components/client'
 
-const Game: NextPage = () => (  
-<Client />
-)
-
+const Game: NextPage = () => {
+  return <Client />
+}
 
 export default Game
