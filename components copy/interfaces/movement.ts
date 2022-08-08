b6bf76b517b62d28b015d0f60edc465b9c049e67
Steffen Hofheinz <shofheinz@stroeer.de>
2022-08-08 15:04:11 +0200

    next steps

--- components copy/interfaces/movement.ts
+++
@@ -1,8 +0,0 @@
-export interface Movement {
-  direction: MovementDirection
-  event: MovementResult
-}
-
-export type MovementDirection = 'n' | 'e' | 's' | 'w'
-
-export type MovementResult = 'fallen' | 'slaughtered' | 'victory' | 'path' | 'invalid'
