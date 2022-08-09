fab294885b4780020bfb13fa979a239131f9b386
Jan Meischner <jan.meischner@stroeer.de>
2022-08-09 16:59:12 +0200

    ✨ use basepath without localhost

--- hooks/api.ts
+++ hooks/api.ts
@@ -1,48 +1,54 @@
-import { MovementDirection, MovementResult } from 'components/interfaces/movement'
+import {
+  MovementDirection,
+  MovementResult,
+} from "components/interfaces/movement";
 
-const basePath = 'http://localhost:3000/api/'
+const basePath = "/api/";
 
 interface ApiResponseNewGame {
-  id: string
+  id: string;
 }
 
 interface ApiResponseMove {
-  movementResult: MovementResult
+  movementResult: MovementResult;
 }
 
-type ApiExecuteFunction<T> = (...args: any[]) => Promise<T>
+type ApiExecuteFunction<T> = (...args: any[]) => Promise<T>;
 
 interface ApiFunctions {
-  newGame: ApiExecuteFunction<ApiResponseNewGame>
-  move: ApiExecuteFunction<ApiResponseMove>
+  newGame: ApiExecuteFunction<ApiResponseNewGame>;
+  move: ApiExecuteFunction<ApiResponseMove>;
 }
 
-const execute = <T>(path: string, method: string, data?: Record<string, unknown>): Promise<T> => {
+const execute = <T>(
+  path: string,
+  method: string,
+  data?: Record<string, unknown>
+): Promise<T> => {
   return new Promise((resolve, reject) => {
-    fetch(
-      `${basePath}${path}`,
-      {
-        method: method.toUpperCase(),
-        mode: 'cors',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: !['GET', 'HEAD'].includes(method.toUpperCase()) ? JSON.stringify(typeof data === 'object' ? data : {}) : undefined
-      }
-    )
-    .then(async (res: Response) => await res.json())
-    .then(json => {
-      resolve(json)
+    fetch(`${basePath}${path}`, {
+      method: method.toUpperCase(),
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: !["GET", "HEAD"].includes(method.toUpperCase())
+        ? JSON.stringify(typeof data === "object" ? data : {})
+        : undefined,
     })
-    .catch(reject)
-  })
-}
+      .then(async (res: Response) => await res.json())
+      .then((json) => {
+        resolve(json);
+      })
+      .catch(reject);
+  });
+};
 
 export const api: ApiFunctions = {
-    newGame: async () => execute<ApiResponseNewGame>('newgame', 'POST'),
-    move: async (sessionId: string | null, direction: MovementDirection) => execute<ApiResponseMove>('move', 'POST', { sessionId, direction })
-  }
+  newGame: async () => execute<ApiResponseNewGame>("newgame", "POST"),
+  move: async (sessionId: string | null, direction: MovementDirection) =>
+    execute<ApiResponseMove>("move", "POST", { sessionId, direction }),
+};
 
-
-export default api
+export default api;
